7b8dc42de60cde3b538a3daaf22e8c4485396088
Ramón Luque <rrl.luque@gmail.com>
2024-01-31 22:51:25 +0100

    Avanzado montaje de semilla con Vite. Aún  por verificación de typescript. Errores con librería vite-plugin-checker en su versión 0.6.3 que no ocurrían en la 0.6.2.

--- bonus/src/index.js
+++
@@ -1,4 +0,0 @@
-const user = "John Doe";
-
-console.log(`Hello ${user}!`);
-console.log("Using Vite");
---
+++ bonus/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
+import typescript from "@rollup/plugin-typescript";
+
+export default defineConfig({
+  plugins: [checker({ typescript: true })],
+  build: {
+    rollupOptions: {
+      plugins: [
+        typescript({
+          tsconfig: "./tsconfig.json",
+        }),
+      ],
+    },
+  },
+});
